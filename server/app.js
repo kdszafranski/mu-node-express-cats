a194a45ec0721c0e0d6092183ed706a86614b8b5
Kris Szafranski <kdszafranski@gmail.com>
2016-05-13 08:47:50 -0500

    fixed port issue, removed facicon fix

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
+// process.env.PORT is for Heroku
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({extended: true}));
